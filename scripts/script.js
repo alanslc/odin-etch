5ec2932a3f665f561001987a8837de45ef4c5050
Alan Chan <alanslc78@hotmail.com>
2022-04-18 09:04:33 +0800

    Add grid to container div (no effect)

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,37 @@
 const INIT_NUM_GRID = 16;
+const MAX_NUM_GRID = 100;
+const MIN_NUM_GRID = 1;
+const BOX_GAP = 2;
 
 let numGrid = INIT_NUM_GRID;
+let curWinW, curWinH, curGridW, curGridH;
+
 
 window.addEventListener('resize', reportWindowResize);
 calGridArea();
+addBoxesToGrid(numGrid);
 
 function reportWindowResize(e) {
    calGridArea();
 }
 
+function addBoxesToGrid(n) {
+   const grid = document.querySelector('.grid');
+
+   const g = n > MAX_NUM_GRID ? MAX_NUM_GRID : n < MIN_NUM_GRID ? MIN_NUM_GRID : n;
+
+   for (let h = 0; h < g; h++) {
+      for (let w = 0; w < g; w++) {
+         const div = document.createElement('div');
+         div.classList.add('box');
+         grid.appendChild(div);
+      }
+   }
+}
+
 function calGridArea() {
-   winW = window.innerWidth;
-   winH = window.innerHeight;
+   curWinW = window.innerWidth;
+   curWinH = window.innerHeight;
 
    let style, gridWidth, gridHeight, side;
 
@@ -28,18 +48,20 @@ function calGridArea() {
    const buttonMarginTop = toPixel(style.marginTop);
    const buttonMarginBottom = toPixel(style.marginBottom);
 
-   gridHeight = winH - bodyPaddingTop - bodyPaddingBottom - buttonHeight - buttonMarginTop - buttonMarginBottom;
-   gridWidth = winW - bodyPaddingLeft - bodyPaddingRight;
+   gridWidth = curWinW - bodyPaddingLeft - bodyPaddingRight;
+   gridHeight = curWinH - bodyPaddingTop - bodyPaddingBottom - buttonHeight - buttonMarginTop - buttonMarginBottom;
 
    const grid = document.querySelector('.grid');
    style = window.getComputedStyle(grid);
    if (gridWidth < gridHeight) {
       grid.style.height = gridWidth + 'px';
       grid.style.width = gridWidth + 'px';
+      curGridW = curGridH = gridWidth;
    }
    else {
       grid.style.height = gridHeight + 'px';
       grid.style.width = gridHeight + 'px';
+      curGridW = curGridH = gridHeight;
    }
 }
 
